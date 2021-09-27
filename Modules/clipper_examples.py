52f82d36e5a6ca4fd216e5203d425f8ee4061da3
zyx <ygpfr74@hotmail.com>
2021-09-27 18:48:45 +0800

    增加 polygon_offset() 检测“对多边形进行放大或者缩小”

--- Modules/clipper_examples.py
+++ Modules/clipper_examples.py
@@ -17,20 +17,48 @@ import pyclipper
 
 
 def two_polygons_intersection(clip, subject):
-    # 主体(subject)和裁剪(clip)的多边形
-    # Difference（相剪）时使用 subject 消除 clip 的部分
-    # 其他 boolean 操作，两个没有区别
+    """
+    两个多边形取交
+    :param clip:裁剪多边形
+    :param subject:主体多边形
+    Difference（相剪）时使用 subject 消除 clip 的部分；其他 boolean 操作，两个没有区别
+    :return: 返回相交后的结果
+    """
     pc = pyclipper.Pyclipper()
     pc.AddPath(clip, pyclipper.PT_CLIP, True)
     pc.AddPath(subject, pyclipper.PT_SUBJECT, True)
     # pc.AddPaths(subject, pyclipper.PT_SUBJECT, True)
+    # http://www.angusj.com/delphi/clipper/documentation/Docs/_Body.htm
+    # CT=ClipType
+    # PFT=PolyFillType
     solution = pc.Execute(pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD)
     return solution
 
 
+def polygon_offset(poly):
+    """
+    对多边形进行放大或者缩小
+    :param poly:
+    :param line_width:
+    :return:
+    """
+    pco = pyclipper.PyclipperOffset()
+    # JT=JoinType(JT_SQUARE：方形倒角；JT_MITER:设定阈值，方形倒角；JT_ROUND：圆形倒角)
+    # 　　jtMiter：对于斜接式交点来说，有必要设定一个阀值，因为偏置的轮廓在极其窄的相交点处可能会造成“尖角”。为了将这些潜在的尖角包含在内，ClippperOffset对象的MiterLimit属性用来制定一个偏置点所能容忍的最大值；对于所有给定的边缘交点处，一旦斜接式交点超过了该阀值，那么方形交角将会被应用；
+    # 　　jtRound：当扁平的路径始终无法完美的获取角度信息，他们等价于一系列的圆弧曲线(可以查阅ClipperObject和ArcTolerance属性)
+    # 　　jtSquare：相当于斜接式当中的delta值始终为1的情况；
+    # ET=EndType
+    # 　　　　etClosedPolygon：末点是相交的，并且使用了JoinType来使路径视作一个多边形填充；
+    # 　　　　etClosedLine：末点是相交的，并且使用了JoinType来使路径视作一条线进行填充；
+    # 　　　　etOpenSqure：末点使用方形尾和扩展的delta角度；
+    # 　　　　etOpenRound：末点使用圆形尾和扩展的delta角度；
+    # 　　　　etOpenButt：末点使用了方向尾，没有任何扩展；
+    pco.AddPath(poly, pyclipper.JT_MITER, pyclipper.ET_CLOSEDPOLYGON)
+    return pco.Execute(2)
+
+
 def main(name):
     print(f'Hi, {name}', datetime.now())
-
 
     pass
 
--- Modules/test_clipper.py
+++ Modules/test_clipper.py
@@ -13,32 +13,39 @@
 """
 from datetime import datetime
 
+from Modules.clipper_examples import polygon_offset
 from Modules.clipper_examples import two_polygons_intersection
+
+# 被裁剪使用的多边形，的部分会在相减时被放弃掉
+subject = (
+        ((180, 200), (260, 200), (260, 150), (180, 150)),
+        ((215, 160), (230, 190), (200, 190))
+)
+
+# 裁剪使用的多边形，即边界
+clip1 = ((190, 210), (240, 210), (240, 130), (190, 130))
+clip2 = ((240, 210), (190, 210), (190, 130), (240, 130))
+clip3 = ((240, 130), (240, 210), (190, 210), (190, 130))
 
 
 def test_two_polygons_intersection():
-    # 被裁剪使用的多边形，的部分会在相减时被放弃掉
-    subject = (
-            ((180, 200), (260, 200), (260, 150), (180, 150)),
-            ((215, 160), (230, 190), (200, 190))
-    )
+    assert two_polygons_intersection(clip1, subject[0]) == [[[240, 200], [190, 200], [190, 150], [240, 150]]]
+    assert two_polygons_intersection(clip2, subject[0]) == [[[240, 200], [190, 200], [190, 150], [240, 150]]]
+    assert two_polygons_intersection(clip3, subject[0]) == [[[240, 200], [190, 200], [190, 150], [240, 150]]]
+    assert two_polygons_intersection(clip1, subject[1]) == [[[230, 190], [200, 190], [215, 160]]]
 
-    # 裁剪使用的多边形，即边界
-    clip = ((190, 210), (240, 210), (240, 130), (190, 130))
 
-    # print(two_polygons_intersection(clip, subject))
-    # [
-    #         [[240, 200], [190, 200], [190, 150], [240, 150]],
-    #         [[200, 190], [230, 190], [215, 160]]
-    # ]
+def test_polygon_offset():
+    assert polygon_offset(clip1) == [[[242, 212], [188, 212], [188, 128], [242, 128]]]
+    assert polygon_offset(clip2) == [[[242, 212], [188, 212], [188, 128], [242, 128]]]
+    assert polygon_offset(clip3) == [[[242, 212], [188, 212], [188, 128], [242, 128]]]
 
-    assert two_polygons_intersection(clip, subject[0]) == [[[240, 200], [190, 200], [190, 150], [240, 150]]]
-    assert two_polygons_intersection(clip, subject[1]) == [[[230, 190], [200, 190], [215, 160]]]
 
-    def main(name):
-        print(f'Hi, {name}', datetime.now())
-        pass
+def main(name):
+    print(f'Hi, {name}', datetime.now())
+    pass
 
-    if __name__ == "__main__":
-        __author__ = 'zYx.Tom'
-        main(__author__)
+
+if __name__ == "__main__":
+    __author__ = 'zYx.Tom'
+    main(__author__)
